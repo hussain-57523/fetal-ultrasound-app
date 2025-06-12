54893809af910cf8311f7c3fc5a81b5d7cfee28b
Hussain-Innovator <hussainsamdani5686@gmail.com>
2025-06-13 04:15:58 +0500

    Updated files: added Streamlit support and cleaned up old files

--- config/config.py
+++
@@ -1,5 +0,0 @@
-import torch
-
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-MODEL_PATH = "model/trained_models/best_model.pth"
-TARGET_NAMES = ['Fetal abdomen', 'Fetal brain', 'Fetal femur', 'Fetal thorax', 'Maternal cervix', 'Other']
